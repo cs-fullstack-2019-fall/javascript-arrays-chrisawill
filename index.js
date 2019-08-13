e34eeeb36d74e1a5488f51fd6cf0180ff27f4b7a
Christian Williams <chrisawill05@gmail.com>
2019-08-13 12:17:27 -0500

    Completed Assigntment for Morning5

---
+++ index.js
@@ -0,0 +1,37 @@
+// #### Exercise 1
+// 1. Create an array.
+// 2. Add four names to the array.
+// 3. Print the array
+// let petNames = ["Coffee","Copper", "Penny","Squirtle"];
+// console.log(petNames);
+
+
+// #### Exercise 2
+// 1. Create an array with 5 numbers in it.
+// 2. Print the numbers.
+// let userNum = [0,1,2,3,4];
+// console.log(userNum);
+// alert(userNum);
+//
+
+
+
+//     #### Exercise 3
+// 1. Create an array with 4 names.
+// 2. Print the third item in the array.
+// 3. Delete the second item.
+// 4. Print the third item in the array again.
+
+let catNames = ["Missy","Excalipurr","Seiko","C H O N K"];
+console.log(catNames);
+catalert(3,catNames);
+function catalert(arrayindex,arrayname)
+{
+    alert(arrayname[arrayindex])
+}
+catNames.pop(2);
+// catAlert(index,catNames );
+// function catAlert(in)
+// {
+//     al
+// }
